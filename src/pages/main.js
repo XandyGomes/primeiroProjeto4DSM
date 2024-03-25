ddd588516308c3b76b089faaafbb79a900178af1
XandyGomes <x3.computadores@hotmail.com>
2024-03-25 20:34:12 -0300

    Aula.25.03.2024

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../services/api';
-import {Keyboard} from 'react-native';
+import {Keyboard, ActivityIndicator} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import {
   Container,
@@ -21,32 +22,56 @@ export default class Main extends Component {
   state = {
     newUser: '',
     users: [],
+    loading: false,
   };
 
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({users: JSON.parse(users)});
+    }
+  }
+
+  async componentDidUpdate(_, prevState) {
+    const {users} = this.state;
+
+    if (prevState.users !== users) {
+      await AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
+
   handleAddUser = async () => {
-    const {users, newUser} = this.state;
+    try {
+      const {users, newUser} = this.state;
+      this.setState({loading: true});
 
-    const response = await api.get(`/users/${newUser}`);
+      const response = await api.get(`/users/${newUser}`);
 
-    const data = {
-      name: response.data.name,
-      login: response.data.login,
-      bio: response.data.bio,
-      avatar: response.data.avatar_url,
-    };
+      const data = {
+        name: response.data.name,
+        login: response.data.login,
+        bio: response.data.bio,
+        avatar: response.data.avatar_url,
+      };
 
-    this.setState({
-      users: [...users, data],
-      newUser: '',
-    });
+      this.setState({
+        users: [...users, data],
+        newUser: '',
+        loading: false,
+      });
 
-    Keyboard.dismiss();
-
-    console.log(data);
+      Keyboard.dismiss();
+    } catch (error) {
+      alert('Usuário não encontrado!');
+      this.setState({
+        loading: false,
+      });
+    }
   };
 
   render() {
-    const {users, newUser} = this.state;
+    const {users, newUser, loading} = this.state;
 
     return (
       <Container>
@@ -60,8 +85,12 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#fff" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={20} color="#fff" />
+            )}
           </SubmitButton>
         </Form>
 
@@ -77,6 +106,17 @@ export default class Main extends Component {
 
               <ProfileButton onPress={() => {}}>
                 <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+              <ProfileButton
+                onPress={() => {
+                  this.setState({
+                    users: this.state.users.filter(
+                      user => user.login !== item.login,
+                    ),
+                  });
+                }}
+                style={{backgroundColor: '#FFC0CB'}}>
+                <ProfileButtonText>Excluir</ProfileButtonText>
               </ProfileButton>
             </User>
           )}
